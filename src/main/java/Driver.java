3cdd557fde57fe8cfd512f31f0f0aee92c042cee
Benas Pakonis <37660999+benaspakonis@users.noreply.github.com>
2020-11-24 20:34:21 +0000

    Expanded Number of Students,Modules and Course

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -29,27 +29,106 @@ public class Driver {
 	private static String LINE_BREAK = "-----------------------------";
 	
 	public static void main(String args[]) {
-		Student student = new Student("Johnny Smith", new DateTime(1999,5,26,0,0));
+		List<Student> allStudents = new ArrayList<Student>();
+		List<Module> allModules = new ArrayList<Module>();
+		List<Course> allCourses = new ArrayList<Course>();
 		
-		List<Student> students = new ArrayList<Student>();
-		students.add(student);
+		//CSIT Students
+		Student student1 = new Student("Johnny Smith", new DateTime(1999,5,26,0,0));
+		Student student2 = new Student("Helen Walsh", new DateTime(1997,8,14,0,0));
+		Student student3 = new Student("Jim O'Neil", new DateTime(1998,1,5,0,0));
 		
-		Module module = new Module("CT401",students);
+		List<Student> csitStudents = new ArrayList<Student>();
+		csitStudents.add(student1);
+		csitStudents.add(student2);
+		csitStudents.add(student3);
 		
-		List<Module> modules = new ArrayList<Module>();
-		modules.add(module);
+		//ECE Students
+		Student student4 = new Student("Sarah Philips", new DateTime(1999,6,21,0,0));
+		Student student5 = new Student("Amy Robinson", new DateTime(2000,11,25,0,0));
+		
+		List<Student> eceStudents = new ArrayList<Student>();
+		eceStudents.add(student4);
+		eceStudents.add(student5);
+		
+		allStudents.add(student1);
+		allStudents.add(student2);
+		allStudents.add(student3);
+		allStudents.add(student4);
+		allStudents.add(student5);
+		
+		
+		Module module1 = new Module("CT401",allStudents);	//CSIT Module
+		Module module2 = new Module("CT402",allStudents);	//CSIT & ECE Module
+		Module module3 = new Module("CT3001",allStudents);	//ECE Module
+		
+		//Adding Modules to Students
+		//CSIT Students
+		student1.addModule(module1);
+		student1.addModule(module2);
+		student2.addModule(module1);
+		student2.addModule(module2);
+		student3.addModule(module1);
+		student3.addModule(module2);
+		
+		//ECE Students
+		student4.addModule(module2);
+		student4.addModule(module3);
+		student5.addModule(module2);
+		student5.addModule(module3);
+		
+		allModules.add(module1);
+		allModules.add(module2);
+		allModules.add(module3);
 		
 		DateTime startDate = new DateTime(2020,9,03,0,0);
 		DateTime endDate = new DateTime(2020,12,3,0,0);
 		
-		Course course = new Course("CSIT",modules,students,startDate,endDate);
+		Course course1 = new Course("CSIT",allModules,allStudents,startDate,endDate);
+		Course course2 = new Course("ECE",allModules,allStudents,startDate,endDate);
 		
-		student.addModule(module);
-		student.addCourse(course);
+		//Adding Modules to Courses
+		course1.addModule(module1);
+		course1.addModule(module2);
+		course2.addModule(module2);
+		course2.addModule(module3);
 		
-		printStudentInfo(student);
-		printModuleInfo(module);
-		printCourseInfo(course);
+		//Adding Students To Courses
+		course1.addStudent(student1);
+		course1.addStudent(student2);
+		course1.addStudent(student3);
+		
+		course2.addStudent(student4);
+		course2.addStudent(student5);
+		
+		//Adding Courses To Modules
+		module1.addCourse(course1);
+		module2.addCourse(course1);
+		module2.addCourse(course2);
+		module3.addCourse(course2);
+		
+		allCourses.add(course1);
+		allCourses.add(course2);
+		
+		//Adding Course To Students
+		student1.addCourse(course1);
+		student2.addCourse(course1);
+		student3.addCourse(course1);
+		student4.addCourse(course2);
+		student5.addCourse(course2);
+		
+		for(int i = 0; i < allStudents.size();i++) {
+			printStudentInfo(allStudents.get(i));
+		}
+		
+		for(int i = 0; i < allModules.size();i++) {
+			printModuleInfo(allModules.get(i));
+		}
+		
+		for(int i = 0; i < allCourses.size();i++) {
+			printCourseInfo(allCourses.get(i));
+		}
+	
 	}
 	
 	public static void printStudentInfo(Student student) {
